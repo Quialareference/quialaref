04b90b0d77bfdf8fd1c796e85598efeb39af74d8
angelcoderrr <angel.martin@jacqui.supply>
2026-03-29 16:54:43 +0200

    Move auth button to left side of header

--- components/Header.tsx
+++ components/Header.tsx
@@ -30,6 +30,27 @@ export function Header() {
           {/* Left */}
           <div className="flex items-center gap-2 w-44">
             <ThemeToggle />
+            {session?.user ? (
+              <Link
+                href="/settings"
+                className="flex items-center gap-1.5 text-xs font-bold bg-white text-gray-900 hover:bg-yellow-300 transition-colors px-3 py-1.5 rounded-full whitespace-nowrap max-w-[120px]"
+              >
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-3.5 h-3.5 shrink-0">
+                  <path fillRule="evenodd" d="M7.5 6a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM3.751 20.105a8.25 8.25 0 0 1 16.498 0 .75.75 0 0 1-.437.695A18.683 18.683 0 0 1 12 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 0 1-.437-.695Z" clipRule="evenodd" />
+                </svg>
+                <span className="truncate">{session.user.username ?? session.user.email?.split("@")[0]}</span>
+              </Link>
+            ) : (
+              <Link
+                href="/auth/signin"
+                className="flex items-center gap-1.5 text-xs font-bold bg-white text-gray-900 hover:bg-yellow-300 transition-colors px-3 py-1.5 rounded-full whitespace-nowrap"
+              >
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-3.5 h-3.5">
+                  <path fillRule="evenodd" d="M7.5 6a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM3.751 20.105a8.25 8.25 0 0 1 16.498 0 .75.75 0 0 1-.437.695A18.683 18.683 0 0 1 12 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 0 1-.437-.695Z" clipRule="evenodd" />
+                </svg>
+                Se connecter
+              </Link>
+            )}
           </div>
 
           {/* Logo centré */}
@@ -62,27 +83,6 @@ export function Header() {
             >
               Parcourir
             </a>
-            {session?.user ? (
-              <Link
-                href="/settings"
-                className="flex items-center gap-1.5 text-xs font-bold bg-white text-gray-900 hover:bg-yellow-300 transition-colors px-3 py-1.5 rounded-full whitespace-nowrap max-w-[120px]"
-              >
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-3.5 h-3.5 shrink-0">
-                  <path fillRule="evenodd" d="M7.5 6a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM3.751 20.105a8.25 8.25 0 0 1 16.498 0 .75.75 0 0 1-.437.695A18.683 18.683 0 0 1 12 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 0 1-.437-.695Z" clipRule="evenodd" />
-                </svg>
-                <span className="truncate">{session.user.username ?? session.user.email?.split("@")[0]}</span>
-              </Link>
-            ) : (
-              <Link
-                href="/auth/signin"
-                className="flex items-center gap-1.5 text-xs font-bold bg-white text-gray-900 hover:bg-yellow-300 transition-colors px-3 py-1.5 rounded-full whitespace-nowrap"
-              >
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-3.5 h-3.5">
-                  <path fillRule="evenodd" d="M7.5 6a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM3.751 20.105a8.25 8.25 0 0 1 16.498 0 .75.75 0 0 1-.437.695A18.683 18.683 0 0 1 12 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 0 1-.437-.695Z" clipRule="evenodd" />
-                </svg>
-                Se connecter
-              </Link>
-            )}
           </div>
 
         </div>
