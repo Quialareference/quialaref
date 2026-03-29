13e8931eb714a2ba3e0c06b99eaa852d7c013703
angelcoderrr <angel.martin@jacqui.supply>
2026-03-29 16:34:21 +0200

    Route wikiref.fr to /wiki

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,30 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/",
+        destination: "/wiki",
+        has: [{ type: "host", value: "wikiref.fr" }],
+      },
+      {
+        source: "/:path*",
+        destination: "/wiki/:path*",
+        has: [{ type: "host", value: "wikiref.fr" }],
+      },
+      {
+        source: "/",
+        destination: "/wiki",
+        has: [{ type: "host", value: "www.wikiref.fr" }],
+      },
+      {
+        source: "/:path*",
+        destination: "/wiki/:path*",
+        has: [{ type: "host", value: "www.wikiref.fr" }],
+      },
+    ];
+  },
   images: {
     remotePatterns: [
       {
